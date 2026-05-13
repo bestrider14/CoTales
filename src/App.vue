184c862587334252695491c8b43098787a35333d
mariecloe28 <marie.cloe@hotmail.com>
2026-05-13 17:45:40 -0400

    style: remove useless css thing

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <v-app class="app-shell">
     <Navbar />
-    <v-main>
+    <v-main class="main-content">
       <RouterView />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
-import Navbar from './components/Layout/Navbar.vue';
+  import { RouterView } from 'vue-router';
+  import Navbar from './components/Layout/Navbar.vue';
 </script>
 
 <style scoped>
-.app-shell {
-  background: var(--app-background);
-}
+  .app-shell {
+    background-color: var(--main-background-color);
+  }
+
+  .main-content {
+    min-height: 100vh;
+    width: 100%;
+  }
 </style>
