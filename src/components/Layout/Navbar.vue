24c2a546d18e87cb380355daab313fb8ddcb2c56
mariecloe28 <marie.cloe@hotmail.com>
2026-05-13 17:47:08 -0400

    style: change the color of the font of the Navbar

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,16 +1,15 @@
 <template>
   <v-app-bar flat class="app-bar">
-    <v-container class="d-flex align-center justify-space-between">
+    <v-container fluid class="d-flex align-center justify-space-between px-4 px-md-8">
       <div class="d-flex align-center ga-3">
         <img alt="CoTales logo" class="brand-logo" src="../../assets/logo.svg" />
         <div>
           <div class="brand-title">CoTales</div>
-          <div class="brand-subtitle">Bibliotheque d'histoires connectees</div>
         </div>
       </div>
 
       <div class="d-none d-md-flex align-center ga-2">
-        <v-btn v-for="link in links" :key="link.to" :to="link.to" variant="text" rounded="pill">
+        <v-btn class="btn-dark" v-for="link in links" :key="link.to" :to="link.to" variant="text" rounded="pill">
           {{ link.label }}
         </v-btn>
       </div>
@@ -42,11 +41,7 @@ const links = [
   font-size: 1rem;
   font-weight: 800;
   line-height: 1.1;
-  color: var(--text-soft);
+  color: var(--accent-color-3)
 }
 
-.brand-subtitle {
-  font-size: 0.8rem;
-  color: var(--text-soft);
-}
 </style>
